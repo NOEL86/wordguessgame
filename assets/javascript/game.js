d68f7ba4c89e3c062ff2a659e2a440f098ae3a37
Ashley <anoelj86@yahoo.com>
2018-04-28 17:20:18 -0600

    html updates

---
+++ assets/javascript/game.js
@@ -0,0 +1,39 @@
+// press a key to start the game
+// display wins
+// display current score
+// display remaining guesses
+// display letters guessed
+// display dashes indicating how many letters (ie: HITMAN = ___ ___ );
+// display a hint button******* BONUS
+
+
+
+// Word is chosen by random in an array
+// Player hits keys to guess hidden letters;
+// player is allowed 10 tries to guess word;
+// if player knows whole word they can enter it;
+// display letters player guessed below;
+// if turns equals zero and word not displayed - player loses;
+// if turns is greater than zero and player guesses word - player wins;
+// hint button shows a letter;
+// if game over prompt to play again !
+// if player guesses a correct letter the tries stay the same
+
+
+// global variables
+var words = ["Labyrinth", "Top Gun", "Goonies", "Ghost Busters"];
+var score = 0;
+document.getElementById("score");
+var guesses = 10;
+document.getElementById("guesses");
+var playAgain = prompt("Want to play again?");
+
+document.onkeyup = function () {
+    confirm("Want to play Hangman?")
+    if (beginGame = true) {
+
+    }
+
+}
+
+
